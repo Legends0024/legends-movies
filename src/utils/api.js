06250a18b0cdb45a0a19518ec22da60f3698bf7c
Manav Joshi <manavjoshi0024@gmail.com>
2026-06-26 16:54:34 +0530

    Restructure repository for Vercel native Serverless API deployment

---
+++ src/utils/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_BASE = "/api";
+
+export const api = {
+  getTrendingMovies: async () => {
+    const res = await axios.get(`${API_BASE}/movies/trending`);
+    return res.data;
+  },
+  getTrendingTvShows: async () => {
+    const res = await axios.get(`${API_BASE}/tv/trending`);
+    return res.data;
+  },
+  search: async (query) => {
+    const res = await axios.get(`${API_BASE}/search?q=${encodeURIComponent(query)}`);
+    return res.data;
+  },
+  getMovieDetails: async (id) => {
+    const res = await axios.get(`${API_BASE}/movie/${id}`);
+    return res.data;
+  },
+  getTvDetails: async (id) => {
+    const res = await axios.get(`${API_BASE}/tv/${id}`);
+    return res.data;
+  },
+  getGenres: async (type = "movie") => {
+    const res = await axios.get(`${API_BASE}/genres/${type}`);
+    return res.data;
+  },
+};
